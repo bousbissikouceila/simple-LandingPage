1fa93465ad3249992c4526b03cf98e26aaed4519
Kouceïla BOUSBISSI <bousbissi.kouceila@gmail.com>
2018-09-14 22:07:17 +0100

    Added three section & their styling and style scrollbar for browser that support it and the smooth scroll

--- dist/js/main.js
+++ dist/js/main.js
@@ -21,3 +21,19 @@ $(function() {
     });
     $('open-menu').height($(window).height());
 });
+
+// Smooth Scrolling
+$('.cf a').on('click', function(e) {
+    if (this.hash !== '') {
+        event.preventDefault();
+        const hash = this.hash;
+
+        $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            },
+            800,
+            function() {
+                window.location.hash = hash;
+            });
+    }
+});
